bc890ca1e7ef59a3325f904d1b34b32b8dbbb7b2
Elkana Orbach <optimove.develop.mobile@gmail.com>
2018-05-29 16:20:05 +0300

    Initial Commit

---
+++ OptimoveSDK/OptimoveSDK.h
@@ -0,0 +1,19 @@
+//
+//  OptimoveSDK.h
+//  OptimoveSDK
+//
+//  Created by Elkana Orbach on 29/05/2018.
+//  Copyright © 2018 Optimove. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OptimoveSDK.
+FOUNDATION_EXPORT double OptimoveSDKVersionNumber;
+
+//! Project version string for OptimoveSDK.
+FOUNDATION_EXPORT const unsigned char OptimoveSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OptimoveSDK/PublicHeader.h>
+
+
